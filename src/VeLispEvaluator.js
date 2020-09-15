e8bec0da48c66f3a488ce316d93e5cb58b5ed50b
Dmitry Klionsky <dm.klionsky@gmail.com>
2020-09-15 20:29:31 +0300

    Cleanup

--- src/VeLispEvaluator.js
+++ src/VeLispEvaluator.js
@@ -4,15 +4,6 @@ const {VeLispParser} = require('../grammar/VeLispParser.js');
 const {VeLispGlobalContext} = require('./VeLispGlobalContext.js');
 const {EvalVisitor} = require('./VeLispEvalVisitor.js');
 
-//const input = '(princ 2)'; // 2
-//const input = '(princ 2.0)'; // 2.0
-//const input = '(princ "2.0")'; // "2.0"
-//const input = '(princ (= 1 1))'; // T
-//const input = '(princ (= 0 1))'; // nil
-//const input = '(princ \'foo)'; // 'foo
-//const input = '(princ (list 1 2 3))'; // (1 2 3)
-//const input = '(princ)'; // ""
-
 function evaluate(input, context = new VeLispGlobalContext()) {
     const chars = new antlr4.InputStream(input);
     const lexer = new VeLispLexer(chars);
