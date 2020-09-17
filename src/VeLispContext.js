32daf14dc2797f2fbaacfa19972f53364b379015
Dmitry Klionsky <dm.klionsky@gmail.com>
2020-09-17 10:15:35 +0300

    VeLisp is a Lisp-1

--- src/VeLispContext.js
+++ src/VeLispContext.js
@@ -1,16 +1,16 @@
 const {Bool} = require('./VeLispTypes.js');
 
+// VeLisp is a Lisp-1
 class VeLispContext {
     constructor(parent = null) {
         this.parent = parent;
-        this.syms = {};
-        this.vars = {};
+        this.symbols = {};
     }
 
     // Init variable in the current context only.
     initVar(name, value) {
         //console.log(`initVar(${name}, ${value})`, this);
-        this.vars[name] = value;
+        this.symbols[name] = value;
     }
 
     // Set variable into the current context, if defined.
@@ -18,12 +18,12 @@ class VeLispContext {
     // Otherwise, set into the current (global) context.
     setVar(name, value) {
         //console.log(`setVar(${name}, ${value})`, this);
-        if (this.vars[name]) {
-            this.vars[name] = value;
+        if (this.symbols[name]) {
+            this.symbols[name] = value;
         } else if (this.parent) {
             this.parent.setVar(name, value);
         } else {
-            this.vars[name] = value;
+            this.symbols[name] = value;
         }
     }
 
@@ -31,7 +31,7 @@ class VeLispContext {
     // If not defined, get variable from the parent context, if available.
     // Otherwise, return nil.
     getVar(name) {
-        let value = this.vars[name];
+        let value = this.symbols[name];
         if (value) {
             return value;
         }
@@ -43,14 +43,14 @@ class VeLispContext {
 
     // Set/Init variable in the current context only.
     setSym(name, value) {
-        this.syms[name] = value;
+        this.symbols[name] = value;
     }
 
     // Get symbol from the current context, if defined.
     // If not defined, get symbol from the parent context, if available.
     // Otherwise, return nil.
     getSym(name) {
-        let value = this.syms[name];
+        let value = this.symbols[name];
         if (value) {
             return value;
         }
