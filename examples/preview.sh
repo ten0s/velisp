968714874bf00d502184735588bc3cf263bc6505
Dmitry Klionsky <dm.klionsky@gmail.com>
2020-11-28 14:38:16 +0300

    Improve script

--- examples/preview.sh
+++ examples/preview.sh
@@ -2,20 +2,27 @@
 
 [[ ${DEBUG} != "" ]] && opts[k++]=--inspect-brk
 
-if [[ $# -ne 1 ]]; then
-    echo "Usage: preview.sh <NAME>"
+if [[ $# -lt 1 ]] || [[ $# -gt 2 ]] ; then
+    echo "Usage: preview.sh <DCL_FILE> [DLG_ID]"
     exit 1
 fi
 
-name=${1}
+dcl_file=${1}
 
-node ${opts[@]} src/main.js <<EOF
-  (setq dcl_file "examples/$name.dcl")
+if [[ ! -z ${2} ]]; then
+    dlg_id=${2}
+else
+    dlg_id=${dcl_file##*/}
+    dlg_id=${dlg_id%%.dcl}
+fi
+
+velisp <<EOF
+  (setq dcl_file "$dcl_file")
   (if (< (setq dcl_id (load_dialog dcl_file)) 0)
     (progn
       (princ (strcat "Error: dcl file '" dcl_file "' not loaded"))
       (exit 1)))
-  (setq dlg_id "$name")
+  (setq dlg_id "$dlg_id")
   (if (not (new_dialog dlg_id dcl_id))
     (progn
       (princ (strcat "Error: dialog '" dlg_id "' not found"))
