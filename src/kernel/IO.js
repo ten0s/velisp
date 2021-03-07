eae48748b179637b127c697422a39e547af0c7ea
Dmitry Klionsky <dm.klionsky@gmail.com>
2021-03-07 19:49:10 +0300

    Cleanup

--- src/kernel/IO.js
+++ src/kernel/IO.js
@@ -76,32 +76,6 @@ exports.initContext = function (context) {
         console.log('\n' + msg + ' ')
         return arg
     }))
-    context.setSym('GETENV', new Fun('getenv', ['name'], [], (self, args) => {
-        if (args.length === 0) {
-            throw new Error('getenv: too few arguments')
-        }
-        if (args.length > 1) {
-            throw new Error('getenv: too many arguments')
-        }
-        const name = ensureType('getenv:', args[0], [Str])
-        const value = process.env[name.value()]
-        if (typeof value === 'undefined') {
-            return new Bool(false)
-        }
-        return new Str(value)
-    }))
-    context.setSym('SETENV', new Fun('setenv', ['name', 'value'], [], (self, args) => {
-        if (args.length < 2) {
-            throw new Error('setenv: too few arguments')
-        }
-        if (args.length > 2) {
-            throw new Error('setenv: too many arguments')
-        }
-        const name = ensureType('setenv: `name`', args[0], [Str])
-        const value = ensureType('setenv: `value`', args[1], [Str])
-        process.env[name.value()] = value.value()
-        return value
-    }))
     context.setSym('OPEN', new Fun('open', ['filename', 'mode'], [], (self, args) => {
         if (args.length < 2) {
             throw new Error('open: too few arguments')
