f04f8ecc5b712fdbbe4d59f9eea409abccbe347c
Dmitry Klionsky <dm.klionsky@gmail.com>
2021-03-13 20:39:29 +0300

    Refine tests

--- test/open-close.js
+++ test/open-close.js
@@ -1,13 +1,17 @@
 const QUnit = require('qunit')
 const {evaluate} = require('../src/VeLispEvaluator.js')
-const {Bool} = require('../src/VeLispTypes.js')
+const {Bool, Sym} = require('../src/VeLispTypes.js')
 
 const tests = [
-    {test: '(close (open "file.txt" "r"))', result: new Bool(false)},
-    {test: '(close (open "file.txt" "w"))', result: new Bool(false)},
-    {test: '(close (open "file.txt" "a"))', result: new Bool(false)},
-
+    {test: '(type (open "file.txt" "r"))', result: new Sym('file')},
+    {test: '(type (open "file.txt" "w"))', result: new Sym('file')},
+    {test: '(type (open "file.txt" "a"))', result: new Sym('file')},
     {test: '(open "unknown.txt" "r")', result: new Bool(false)},
+    {test: '(type (open "unknown.txt" "a"))', result: new Sym('file')},
+
+    {test: '(close (open "file.txt" "r")))', result: new Bool(false)},
+    {test: '(close (open "file.txt" "w")))', result: new Bool(false)},
+    {test: '(close (open "file.txt" "a")))', result: new Bool(false)},
     {test: '(setq f (open "file.txt" "w")) (close f) (close f)', result: new Bool(false)},
 ]
 
@@ -17,6 +21,7 @@ const errors = [
     {test: '(open "file.txt" "r" \'extra)', result: new Error('open: too many arguments')},
 
     {test: '(close)', result: new Error('close: too few arguments')},
+    {test: '(close 1)', result: new Error('close: `file-desc` expected File')},
     {test: '(close (open "file.txt" "w") \'extra)', result: new Error('close: too many arguments')},
 ]
 
@@ -24,6 +29,7 @@ QUnit.test('open-close', assert => {
     const fs = require('fs')
     // Setup
     fs.closeSync(fs.openSync('file.txt', 'w'))
+    try { fs.unlinkSync('unknown.txt') } catch { }
 
     tests.forEach(t => {
         assert.deepEqual(evaluate(t.test), t.result, t.test)
@@ -31,6 +37,7 @@ QUnit.test('open-close', assert => {
 
     // Tear down
     fs.unlinkSync('file.txt')
+    fs.unlinkSync('unknown.txt')
 
     errors.forEach(t => {
         assert.throws(() => evaluate(t.test), t.result, t.test)
