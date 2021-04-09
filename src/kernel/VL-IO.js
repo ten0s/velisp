8d4e0d46798a327dcbeff163701e3980bb6bb739
Dmitry Klionsky <dm.klionsky@gmail.com>
2021-04-09 17:51:52 +0300

    Implement vl-filename-mktemp

--- src/kernel/VL-IO.js
+++ src/kernel/VL-IO.js
@@ -1,7 +1,74 @@
 const fs = require('fs')
+const path = require('path')
+const temp = require('temp').track()
 const {Bool, Str, Fun, ensureType} = require('../VeLispTypes.js')
 
 exports.initContext = function (context) {
+    context.setSym('VL-FILENAME-MKTEMP', new Fun('vl-filename-mktemp', ['pattern', 'directory', 'extention'], [], (self, args) => {
+        if (args.length > 3) {
+            throw new Error('vl-filename-mktemp: too many arguments')
+        }
+
+        const DEFAULT_NAME = '$VL~~'
+        const DEFAULT_DIR  = tmpDir()
+        const DEFAULT_EXT  = ''
+
+        let dir  = null
+        let name = null
+        let ext  = null
+
+        if (args.length == 1) {
+            let pattern = ensureType('vl-filename-mktemp:', args[0], [Str, Bool])
+            if (pattern instanceof Str) {
+                ({dir, name, ext} = path.parse(pattern.value()))
+                if (!fs.existsSync(dir)) {
+                    dir = null
+                }
+            }
+        }
+        if (args.length == 2) {
+            let pattern   = ensureType('vl-filename-mktemp:', args[0], [Str, Bool])
+            let directory = ensureType('vl-filename-mktemp:', args[1], [Str, Bool])
+            if (pattern instanceof Str) {
+                ({dir, name, ext} = path.parse(pattern.value()))
+                if (!fs.existsSync(dir)) {
+                    dir = null
+                }
+            }
+            if (directory instanceof Str) {
+                dir = directory.value()
+                if (!fs.existsSync(dir)) {
+                    dir = null
+                }
+            }
+        }
+        if (args.length == 3) {
+            let pattern   = ensureType('vl-filename-mktemp:', args[0], [Str, Bool])
+            let directory = ensureType('vl-filename-mktemp:', args[1], [Str, Bool])
+            let extension = ensureType('vl-filename-mktemp:', args[2], [Str, Bool])
+            if (pattern instanceof Str) {
+                ({dir, name, ext} = path.parse(pattern.value()))
+                if (!fs.existsSync(dir)) {
+                    dir = null
+                }
+            }
+            if (directory instanceof Str) {
+                dir = directory.value()
+                if (!fs.existsSync(dir)) {
+                    dir = null
+                }
+            }
+            if (extension instanceof Str) {
+                ext = extension.value()
+            }
+        }
+        const {path: tmpFile} = temp.openSync({
+            dir   : dir  ? dir  : DEFAULT_DIR,
+            prefix: name ? name : DEFAULT_NAME,
+            suffix: ext  ? ext  : DEFAULT_EXT,
+        })
+        return new Str(tmpFile)
+    }))
     context.setSym('VL-FILE-DELETE', new Fun('vl-file-delete', ['filename'], [], (self, args) => {
         if (args.length === 0) {
             throw new Error('vl-file-delete: too few arguments')
@@ -36,4 +103,13 @@ exports.initContext = function (context) {
             return new Bool(false)
         }
     }))
+}
+
+const tmpDir = () => {
+    let tmp = null
+    tmp = process.env["TMP"]
+    if (fs.existsSync(tmp)) { return tmp }
+    tmp = process.env["TEMP"]
+    if (fs.existsSync(tmp)) { return tmp }
+    return process.cwd()
 }
---
+++ test/vlFilenameMkTemp.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const os = require('os')
+const QUnit = require('qunit')
+const {evaluate} = require('../src/VeLispEvaluator.js')
+const {Bool} = require('../src/VeLispTypes.js')
+
+const tests = [
+    //{test: '(vl-filename-mktemp)', result: new Str('')},
+    //{test: '(vl-filename-mktemp nil nil nil)', result: new Str('')},
+]
+
+const errors = [
+    {test: '(vl-filename-mktemp nil nil nil nil)',
+     result: new Error('vl-filename-mktemp: too many arguments')},
+]
+    
+QUnit.test('vl-filename-mktemp', assert => {
+    // Setup
+    // ...
+    
+    tests.forEach(t => {
+        assert.deepEqual(evaluate(t.test), t.result, t.test)
+    })
+
+    errors.forEach(t => {
+        assert.throws(() => evaluate(t.test), t.result, t.test)
+    })
+
+    // Tear down
+    // ...
+})
