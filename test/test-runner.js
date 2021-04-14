f72779541084bff91ea36c42dee2eb9afa2d5940
Dmitry Klionsky <dm.klionsky@gmail.com>
2021-04-14 17:37:24 +0300

    Allow absence of tests and/or errors

--- test/test-runner.js
+++ test/test-runner.js
@@ -3,30 +3,32 @@ const {evaluate} = require('../src/VeLispEvaluator.js')
 
 class TestRunner {
     static run({name = 'test-name', setup = () => {}, teardown = () => {}, tests = [], errors = []}) {
-        QUnit.test(name, assert => {
-            tests.forEach(t => {
-                let env = undefined
-                try { env = setup() } catch (e) {}
+        if (tests.length || errors.length) {
+            QUnit.test(name, assert => {
+                tests.forEach(t => {
+                    let env = undefined
+                    try { env = setup() } catch (e) {}
 
-                const actual = evaluate(t.test)
-                if (typeof(t.result) === 'function') {
-                    assert.ok(t.result(actual, env), t.test)
-                } else {
-                    assert.deepEqual(actual, t.result, t.test)
-                }
+                    const actual = evaluate(t.test)
+                    if (typeof(t.result) === 'function') {
+                        assert.ok(t.result(actual, env), t.test)
+                    } else {
+                        assert.deepEqual(actual, t.result, t.test)
+                    }
 
-                try { teardown(env) } catch (e) {}
+                    try { teardown(env) } catch (e) {}
+                })
+
+                errors.forEach(t => {
+                    let env = undefined
+                    try { env = setup() } catch (e) {}
+
+                    assert.throws(() => evaluate(t.test), t.result, t.test)
+
+                    try { teardown(env) } catch (e) {}
+                })
             })
-
-            errors.forEach(t => {
-                let env = undefined
-                try { env = setup() } catch (e) {}
-
-                assert.throws(() => evaluate(t.test), t.result, t.test)
-
-                try { teardown(env) } catch (e) {}
-            })
-        })
+        }
     }
 }
 
