fb40ba6753a18e1885d3996e9e63d0ec161c6aea
Dmitry Klionsky <dm.klionsky@gmail.com>
2021-04-17 21:16:24 +0300

    Implement Digraph ADT

---
+++ src/VeDigraph.js
@@ -0,0 +1,62 @@
+const {EOL} = require('os')
+
+class VeDigraph {
+    constructor(V) {
+        this._vertices = V
+        this._edges = 0
+        this._adjacent = []
+        for (let v = 0; v < V; v++) {
+            this._adjacent[v] = []
+        }
+    }
+
+    vertices() {
+        return this._vertices
+    }
+
+    edges() {
+        return this._edges
+    }
+
+    addEdge(v, w) {
+        this._adjacent[v].push(w)
+        this._edges++
+    }
+
+    adjacent(v) {
+        return this._adjacent[v]
+    }
+
+    toString() {
+        let s = `${this._vertices} vertices, ${this._edges} edges` + EOL
+        for (let v = 0; v < this._vertices; v++) {
+            s += `${v}: `
+            for (let w of this._adjacent[v]) {
+                s += `${w} `
+            }
+            s += EOL
+        }
+        return s
+    }
+
+    toDot() {
+        let s = 'digraph {' + EOL
+        const nodes = new Set()
+        for (let v = 0; v < this._vertices; v++) {
+            for (let w of this._adjacent[v]) {
+                nodes.add(v)
+                nodes.add(w)
+                s += `  ${v} -> ${w};` + EOL
+            }
+        }
+        for (let v = 0; v < this._vertices; v++) {
+            if (!nodes.has(v)) {
+                s += `  ${v};` + EOL
+            }
+        }
+        s += '}' + EOL
+        return s
+    }
+}
+
+exports.VeDigraph = VeDigraph
---
+++ test/VeDigraph.js
@@ -0,0 +1,44 @@
+const QUnit = require('qunit')
+const {VeDigraph} = require('../src/VeDigraph.js')
+
+QUnit.test('VeDigraph', assert => {
+    const G = new VeDigraph(10)
+    assert.equal(10, G.vertices())
+    assert.equal(0, G.edges())
+
+    G.addEdge(0, 1)
+    G.addEdge(0, 7)
+    G.addEdge(1, 2)
+    G.addEdge(1, 3)
+    G.addEdge(1, 7)
+    G.addEdge(1, 8)
+    G.addEdge(2, 3)
+    G.addEdge(3, 4)
+    G.addEdge(3, 5)
+    G.addEdge(3, 8)
+    G.addEdge(4, 5)
+    G.addEdge(5, 6)
+    G.addEdge(5, 7)
+    G.addEdge(5, 8)
+    G.addEdge(6, 7)
+    G.addEdge(7, 8)
+
+    console.log(G)
+    console.log(G.toString())
+    console.log(G.toDot())
+
+    assert.equal(10, G.vertices())
+    assert.equal(16, G.edges())
+
+    let outdegree0 = 0
+    G.adjacent(0).forEach(_ => outdegree0++)
+    assert.equal(2, outdegree0)
+    
+    let outdegree1 = 0
+    G.adjacent(1).forEach(_ => outdegree1++)
+    assert.equal(4, outdegree1)
+
+    let outdegree8 = 0
+    G.adjacent(8).forEach(_ => outdegree8++)
+    assert.equal(0, outdegree8)
+})
