f1a0fca6d4c404080efa69177f1dc4a19a0f0351
Dmitry Klionsky <dm.klionsky@gmail.com>
2021-04-23 20:53:21 +0300

    Add newlines

--- examples/dcl/multiple_dlg.lsp
+++ examples/dcl/multiple_dlg.lsp
@@ -1,19 +1,19 @@
 (defun new_dlg (dcl_file dlg_id / dcl_id ret)
   (if (< (setq dcl_id (load_dialog dcl_file)) 0)
       (progn
-        (princ (strcat "Error: dcl file '" dcl_file "' not loaded"))
+        (princ (strcat "Error: dcl file '" dcl_file "' not loaded\n"))
         (exit 1)))
 
   (if (not (new_dialog dlg_id dcl_id))
       (progn
-        (princ (strcat "Error: dialog '" dlg_id "' not found"))
+        (princ (strcat "Error: dialog '" dlg_id "' not found\n"))
         (exit 1)))
 
   (action_tile "new_dlg" "(new_dlg dcl_file dlg_id)")
   (action_tile "term_all" "(term_dialog)")
 
   (setq ret (start_dialog))
-  (princ (strcat "dialog done w/ " (itoa ret)))
+  (princ (strcat "dialog done w/ " (itoa ret) "\n"))
   (unload_dialog dcl_id)
 )
 
--- examples/dcl/run_dlg.sh
+++ examples/dcl/run_dlg.sh
@@ -20,21 +20,21 @@ node ${opts[@]} ${dir}/../../src/main.js <<EOF
   (setq dcl_file "${dir}/${name}.dcl")
   (if (< (setq dcl_id (load_dialog dcl_file)) 0)
     (progn
-      (princ (strcat "Error: dcl file '" dcl_file "' not loaded"))
+      (princ (strcat "Error: dcl file '" dcl_file "' not loaded\n"))
       (exit 1)))
 
   (defun default (key value reason)
-    (princ (strcat "default: key: " key " value: " value " reason: " (itoa reason))))
+    (princ (strcat "default: key: " key " value: " value " reason: " (itoa reason) "\n")))
 
   (setq dlg_id "${name}")
   (if (not (new_dialog dlg_id dcl_id "(default \$key \$value \$reason)" '(-1 -1)))
     (progn
-      (princ (strcat "Error: dialog '" dlg_id "' not found"))
+      (princ (strcat "Error: dialog '" dlg_id "' not found\n"))
       (exit 1)))
 
   ${init}
 
   (setq ret (start_dialog))
-  (princ (strcat "dialog done w/ " (itoa ret)))
+  (princ (strcat "dialog done w/ " (itoa ret) "\n"))
   (unload_dialog dcl_id)
 EOF
