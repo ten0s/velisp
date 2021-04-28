0872e9e7829ac7bae8399a70583f8b37b02524fa
Dmitry Klionsky <dm.klionsky@gmail.com>
2021-04-28 18:54:11 +0300

    Test princ

---
+++ test/princ.js
@@ -0,0 +1,20 @@
+const {TestRunner} = require('./test-runner.js')
+const {Bool, Int, Str, List} = require('../src/VeLispTypes.js')
+
+TestRunner.run({
+    name: 'princ',
+
+    tests: [
+        {test: '(princ)', result: new Str('')},
+        {test: '(princ nil)', result: new Bool(false)},
+        {test: '(princ 1)', result: new Int(1)},
+        {test: '(princ "msg")', result: new Str('msg')},
+        {test: '(princ \'(1))', result: new List([new Int(1)])},
+        // See princ.{exp,lsp}
+    ],
+
+    errors: [
+        {test: '(princ "msg" "file" "msg")', result: new Error('princ: too many arguments')},
+        {test: '(princ "msg" "file")', result: new Error('princ: `file-desc` expected File')},
+    ]
+})
---
+++ test/princ.lsp
@@ -0,0 +1,20 @@
+(princ "Test #1\n")
+(princ)
+
+(princ "Test #2\n")
+(princ "\n")
+
+(princ "Test #3\n")
+(princ "Hello: ")
+
+(princ "Test #4\n")
+(princ "Hello: \n")
+
+(princ "Test #5\n")
+(princ "<| \" \r \n \t |>")
+
+(princ "Test #6\n")
+(princ nil)
+
+(princ "Test #7\n")
+(princ '(1 "2" '3 ))
