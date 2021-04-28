1c4007c47607fab9e438d5e6bf742c4098f46b72
Dmitry Klionsky <dm.klionsky@gmail.com>
2021-04-28 19:57:28 +0300

    Add todo

--- test/chr.js
+++ test/chr.js
@@ -6,6 +6,7 @@ const tests = [
     {test: '(chr 65)', result: new Str('A')},
     {test: '(chr 66)', result: new Str('B')},
     {test: '(chr 97)', result: new Str('a')},
+    // TODO: (chr 10) (chr 13) (chr 9)
 ]
 
 const errors = [
