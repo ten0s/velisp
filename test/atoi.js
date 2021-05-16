1804d314cb87eb9ce1671d3b38a2872df25d6e62
Dmitry Klionsky <dm.klionsky@gmail.com>
2021-05-16 20:16:57 +0300

    Review tests

--- test/atoi.js
+++ test/atoi.js
@@ -1,27 +1,20 @@
-const QUnit = require('qunit')
-const {evaluate} = require('../src/VeLispEvaluator.js')
+const {TestRunner} = require('./test-runner.js')
 const {Int} = require('../src/VeLispTypes.js')
 
-const tests = [
-    {test: '(atoi "3")', result: new Int(3)},
-    {test: '(atoi "-17")', result: new Int(-17)},
-    {test: '(atoi "3.9")', result: new Int(3)},
-    {test: '(atoi "")', result: new Int(0)},
-    {test: '(atoi "abc")', result: new Int(0)},
-]
+TestRunner.run({
+    name: 'atoi',
 
-const errors = [
-    {test: '(atoi)', result: new Error('atoi: too few arguments')},
-    {test: '(atoi "1" "2")', result: new Error('atoi: too many arguments')},
-    {test: '(atoi 1)', result: new Error('atoi: expected Str')},
-]
+    tests: [
+        {test: '(atoi "3")', result: new Int(3)},
+        {test: '(atoi "-17")', result: new Int(-17)},
+        {test: '(atoi "3.9")', result: new Int(3)},
+        {test: '(atoi "")', result: new Int(0)},
+        {test: '(atoi "abc")', result: new Int(0)},
+    ],
 
-QUnit.test('atoi', assert => {
-    tests.forEach(t => {
-        assert.deepEqual(evaluate(t.test), t.result, t.test)
-    })
-
-    errors.forEach(t => {
-        assert.throws(() => evaluate(t.test), t.result, t.test)
-    })
+    errors: [
+        {test: '(atoi)', result: new Error('atoi: too few arguments')},
+        {test: '(atoi "1" "2")', result: new Error('atoi: too many arguments')},
+        {test: '(atoi 1)', result: new Error('atoi: expected Str')},
+    ]
 })
--- test/itoa.js
+++ test/itoa.js
@@ -1,24 +1,17 @@
-const QUnit = require('qunit')
-const {evaluate} = require('../src/VeLispEvaluator.js')
+const {TestRunner} = require('./test-runner.js')
 const {Str} = require('../src/VeLispTypes.js')
 
-const tests = [
-    {test: '(itoa 33)', result: new Str('33')},
-    {test: '(itoa -17)', result: new Str('-17')},
-]
+TestRunner.run({
+    name: 'itoa',
 
-const errors = [
-    {test: '(itoa)', result: new Error('itoa: too few arguments')},
-    {test: '(itoa 1 2)', result: new Error('itoa: too many arguments')},
-    {test: '(itoa "1")', result: new Error('itoa: expected Int')},
-]
+    tests: [
+        {test: '(itoa 33)', result: new Str('33')},
+        {test: '(itoa -17)', result: new Str('-17')},
+    ],
 
-QUnit.test('itoa', assert => {
-    tests.forEach(t => {
-        assert.deepEqual(evaluate(t.test), t.result, t.test)
-    })
-
-    errors.forEach(t => {
-        assert.throws(() => evaluate(t.test), t.result, t.test)
-    })
+    errors: [
+        {test: '(itoa)', result: new Error('itoa: too few arguments')},
+        {test: '(itoa 1 2)', result: new Error('itoa: too many arguments')},
+        {test: '(itoa "1")', result: new Error('itoa: expected Int')},
+    ]
 })
