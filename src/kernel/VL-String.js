80f1ce71b481aea2e0cb9b9f3a4e642c5dbea0b1
Dmitry Klionsky <dm.klionsky@gmail.com>
2021-05-20 17:31:44 +0300

    Implement vl-string-position

--- src/kernel/VL-String.js
+++ src/kernel/VL-String.js
@@ -29,6 +29,55 @@ exports.initContext = (context) => {
         }
         return new Str(arg.toString())
     }))
+    context.setSym('VL-STRING-POSITION', new Fun('vl-string-position', ['char-code', 'string', '[start-pos [from-end-p]]'], [], (self, args) => {
+        if (args.length < 2) {
+            throw new Error('vl-string-position: too few arguments')
+        }
+        if (args.length > 4) {
+            throw new Error('vl-string-position: too many arguments')
+        }
+        const code = ensureType('vl-string-position: `char-code`', args[0], [Int]).value()
+        const string = ensureType('vl-string-position: `string`', args[1], [Str]).value()
+        let startPos = 0
+        let fromEnd = false
+        if (args.length === 3) {
+            const arg = ensureType('vl-string-position: `start-pos`', args[2], [Int, Bool])
+            if (arg instanceof Int) {
+                startPos = arg.value()
+            }
+            if (startPos < 0) {
+                throw new Error('vl-string-position: `start-pos` expected non-negative Int')
+            }
+        }
+        if (args.length === 4) {
+            const arg = ensureType('vl-string-position: `start-pos`', args[2], [Int, Bool])
+            if (arg instanceof Int) {
+                startPos = arg.value()
+            }
+            if (startPos < 0) {
+                throw new Error('vl-string-position: `start-pos` expected non-negative Int')
+            }
+            fromEnd = ensureType('vl-string-position: `from-end-p`', args[3], [Bool]).value()
+        }
+        let shifted = string
+        if (startPos > 0) {
+            shifted = string.slice(startPos)
+        }
+        if (!fromEnd) {
+            for (let i = 0; i < shifted.length; i++) {
+                if (shifted.charCodeAt(i) === code) {
+                    return new Int(i + startPos)
+                }
+            }
+        } else {
+            for (let i = shifted.length - 1; i >= 0; i--) {
+                if (shifted.charCodeAt(i) === code) {
+                    return new Int(i + startPos)
+                }
+            }
+        }
+        return new Bool(false)
+    }))
     context.setSym('VL-STRING-SEARCH', new Fun('vl-string-search', ['pattern', 'string', '[start-pos]'], [], (self, args) => {
         if (args.length < 2) {
             throw new Error('vl-string-search: too few arguments')
---
+++ test/vlStringPosition.js
@@ -0,0 +1,47 @@
+const {TestRunner} = require('./test-runner.js')
+const {Bool, Int} = require('../src/VeLispTypes.js')
+
+TestRunner.run({
+    name: 'vl-string-position',
+
+    tests: [
+        {test: '(vl-string-position (ascii "z") "azbdc")', result: new Int(1)},
+        {test: '(vl-string-position 122 "azbzc")', result: new Int(1)},
+        {test: '(vl-string-position (ascii "x") "azbzc")', result: new Bool(false)},
+
+        {test: '(vl-string-position (ascii "z") "azbzc")', result: new Int(1)},
+        {test: '(vl-string-position (ascii "z") "azbzc" 0)', result: new Int(1)},
+        {test: '(vl-string-position (ascii "z") "azbzc" nil)', result: new Int(1)},
+        {test: '(vl-string-position (ascii "z") "azbzc" 2)', result: new Int(3)},
+        
+        {test: '(vl-string-position (ascii "z") "azbzlmnqc" nil T)', result: new Int(3)},
+    ],
+
+    errors: [
+        {test: '(vl-string-position)', result:
+         new Error('vl-string-position: too few arguments')},
+        {test: '(vl-string-position 98)', result:
+         new Error('vl-string-position: too few arguments')},
+
+        {test: '(vl-string-position 98 "bar" 0 nil nil)', result:
+         new Error('vl-string-position: too many arguments')},
+
+        {test: '(vl-string-position "b" "bar")', result:
+         new Error('vl-string-position: `char-code` expected Int')},
+        
+        {test: '(vl-string-position 98 \bar)', result:
+         new Error('vl-string-position: `string` expected Str')},
+
+        {test: '(vl-string-position 98 "bar" "1")', result:
+         new Error('vl-string-position: `start-pos` expected Int, Bool')},
+        {test: '(vl-string-position 98 "bar" -1)', result:
+         new Error('vl-string-position: `start-pos` expected non-negative Int')},
+        {test: '(vl-string-position 98 "bar" "1" nil)', result:
+         new Error('vl-string-position: `start-pos` expected Int, Bool')},
+        {test: '(vl-string-position 98 "bar" -1 nil)', result:
+         new Error('vl-string-position: `start-pos` expected non-negative Int')},
+
+        {test: '(vl-string-position 98 "bar" nil 0)', result:
+         new Error('vl-string-position: `from-end-p` expected Bool')},
+    ]
+})
