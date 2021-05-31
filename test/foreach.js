04f716ee36f2195eb55725dbed7226a5368bf541
Dmitry Klionsky <dm.klionsky@gmail.com>
2021-05-31 12:24:28 +0300

    Refine test

--- test/foreach.js
+++ test/foreach.js
@@ -1,33 +1,34 @@
-const QUnit = require('qunit')
-const {evaluate} = require('../src/VeLispEvaluator.js')
+const {TestRunner} = require('./test-runner.js')
 const {Bool, Int} = require('../src/VeLispTypes.js')
 
-const tests = [
-    {test: '(foreach n (list 1 2 3))', result: new Bool(false)},
-    {test: '(Foreach n (list 1 2 3))', result: new Bool(false)},
-    {test: '(FOreach n (list 1 2 3))', result: new Bool(false)},
-    {test: '(FOReach n (list 1 2 3))', result: new Bool(false)},
-    {test: '(FOREach n (list 1 2 3))', result: new Bool(false)},
-    {test: '(FOREAch n (list 1 2 3))', result: new Bool(false)},
-    {test: '(FOREACh n (list 1 2 3))', result: new Bool(false)},
-    {test: '(FOREACH n (list 1 2 3))', result: new Bool(false)},
+TestRunner.run({
+    name: 'foreach',
 
-    {test: '(foreach n (list))', result: new Bool(false)},
-    {test: '(foreach n \'())', result: new Bool(false)},
-    {test: '(foreach n ())', result: new Bool(false)},
-    {test: '(foreach n nil)', result: new Bool(false)},
+    tests: [
+        {test: '(foreach n (list 1 2 3))', result: new Bool(false)},
+        {test: '(Foreach n (list 1 2 3))', result: new Bool(false)},
+        {test: '(FOreach n (list 1 2 3))', result: new Bool(false)},
+        {test: '(FOReach n (list 1 2 3))', result: new Bool(false)},
+        {test: '(FOREach n (list 1 2 3))', result: new Bool(false)},
+        {test: '(FOREAch n (list 1 2 3))', result: new Bool(false)},
+        {test: '(FOREACh n (list 1 2 3))', result: new Bool(false)},
+        {test: '(FOREACH n (list 1 2 3))', result: new Bool(false)},
 
-    {test: '(foreach n (list 1 2 3) n)', result: new Int(3)},
-    {test: '(foreach n (list 1 2 3) N)', result: new Int(3)},
-    {test: '(foreach N (list 1 2 3) n)', result: new Int(3)},
+        {test: '(foreach n (list))', result: new Bool(false)},
+        {test: '(foreach n \'())', result: new Bool(false)},
+        {test: '(foreach n ())', result: new Bool(false)},
+        {test: '(foreach n nil)', result: new Bool(false)},
 
-    {test: `(setq fac 1)
+        {test: '(foreach n (list 1 2 3) n)', result: new Int(3)},
+        {test: '(foreach n (list 1 2 3) N)', result: new Int(3)},
+        {test: '(foreach N (list 1 2 3) n)', result: new Int(3)},
+
+        {test: `(setq fac 1)
             (foreach n (list 1 2 3 4 5)
               (setq fac (* n fac)))`, result: new Int(120)},
-]
+    ],
 
-QUnit.test('foreach', assert => {
-    tests.forEach(t => {
-        assert.deepEqual(evaluate(t.test), t.result, t.test)
-    })
+    errors: [
+        {test: '(foreach n T)', result: new Error('foreach: `list` expected List')},
+    ]
 })
