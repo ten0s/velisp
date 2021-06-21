9d0dae0fc119bee951d5014b519534a266fbce3c
Dmitry Klionsky <dm.klionsky@gmail.com>
2021-06-21 19:45:53 +0300

    Cleanup

--- test/mapcar.js
+++ test/mapcar.js
@@ -1,49 +1,42 @@
-const QUnit = require('qunit')
-const {evaluate} = require('../src/VeLispEvaluator.js')
+const {TestRunner} = require('./test-runner.js')
 const {Int, List} = require('../src/VeLispTypes.js')
 
-const tests = [
-    {test: '(mapcar \'+ nil)', result: new List([])},
-    {test: '(mapcar \'+ ())', result: new List([])},
-    {test: '(mapcar \'+ \'())', result: new List([])},
+TestRunner.run({
+    name: 'mapcar',
 
-    {test: '(mapcar \'(lambda (x) x) \'(1 2 3))', result: new List([
-        new Int(1), new Int(2), new Int(3)
-    ])},
-    {test: '(mapcar \'+ \'(1 2 3))', result: new List([
-        new Int(1), new Int(2), new Int(3)
-    ])},
-    {test: '(mapcar \'(lambda (x) (* x x)) \'(1 2 3))', result: new List([
-        new Int(1), new Int(4), new Int(9)
-    ])},
+    tests: [
+        {test: '(mapcar \'+ nil)', result: new List([])},
+        {test: '(mapcar \'+ ())', result: new List([])},
+        {test: '(mapcar \'+ \'())', result: new List([])},
 
-    {test: '(mapcar \'+ \'(1 2 3) \'(9 8 7))', result: new List([
-        new Int(10), new Int(10), new Int(10)
-    ])},
-    {test: '(mapcar \'(lambda (x y) (expt x y)) \'(1 2 3) \'(3 3 3))', result: new List([
-        new Int(1), new Int(8), new Int(27)
-    ])},
+        {test: '(mapcar \'(lambda (x) x) \'(1 2 3))', result: new List([
+            new Int(1), new Int(2), new Int(3)
+        ])},
+        {test: '(mapcar \'+ \'(1 2 3))', result: new List([
+            new Int(1), new Int(2), new Int(3)
+        ])},
+        {test: '(mapcar \'(lambda (x) (* x x)) \'(1 2 3))', result: new List([
+            new Int(1), new Int(4), new Int(9)
+        ])},
 
-    // Diff length
-    {test: '(mapcar \'+ \'(1 2 3) \'(4 5 6 7))', result: new List([
-        new Int(5), new Int(7), new Int(9)
-    ])},
-]
+        {test: '(mapcar \'+ \'(1 2 3) \'(9 8 7))', result: new List([
+            new Int(10), new Int(10), new Int(10)
+        ])},
+        {test: '(mapcar \'(lambda (x y) (expt x y)) \'(1 2 3) \'(3 3 3))', result: new List([
+            new Int(1), new Int(8), new Int(27)
+        ])},
 
-const errors = [
-    {test: '(mapcar)', result: new Error('mapcar: too few arguments')},
-    {test: '(mapcar \'+)', result: new Error('mapcar: too few arguments')},
-    {test: '(mapcar "+")', result: new Error('mapcar: too few arguments')},
-    {test: '(mapcar "+" \'())', result: new Error('mapcar: no such function "+"')},
-    {test: '(mapcar \'+ "str")', result: new Error('mapcar: `list` expected List')},
-]
+        // Diff length
+        {test: '(mapcar \'+ \'(1 2 3) \'(4 5 6 7))', result: new List([
+            new Int(5), new Int(7), new Int(9)
+        ])},
+    ],
 
-QUnit.test('mapcar', assert => {
-    tests.forEach(t => {
-        assert.deepEqual(evaluate(t.test), t.result, t.test)
-    })
-
-    errors.forEach(t => {
-        assert.throws(() => evaluate(t.test), t.result, t.test)
-    })
+    errors: [
+        {test: '(mapcar)', result: new Error('mapcar: too few arguments')},
+        {test: '(mapcar \'+)', result: new Error('mapcar: too few arguments')},
+        {test: '(mapcar "+")', result: new Error('mapcar: too few arguments')},
+        {test: '(mapcar "+" \'())', result: new Error('mapcar: no such function "+"')},
+        {test: '(mapcar \'+ "str")', result: new Error('mapcar: `list` expected List')},
+    ]
 })
