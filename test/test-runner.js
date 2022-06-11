96f382c7b8ef2d523c92a51ad4e576228d68e2ad
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-06-11 18:41:05 +0300

    Support platform specific tests in TestRunner

--- test/test-runner.js
+++ test/test-runner.js
@@ -2,33 +2,94 @@ import QUnit from 'qunit'
 import {evaluate} from '../src/VeLispEvaluator.js'
 
 class TestRunner {
-    static run({name = 'test-name', setup = () => {}, teardown = () => {}, tests = [], errors = []}) {
-        if (tests.length || errors.length) {
+    static run({
+        name = 'test-name',
+
+        setup = () => {},
+        teardown = () => {},
+
+        tests = [],
+        testsLinux = [],
+        testsWin = [],
+        testsMac = [],
+
+        errors = [],
+        errorsLinux = [],
+        errorsWin = [],
+        errorsMac = [],
+
+    }) {
+        if (tests.length       ||
+            testsLinux.length  ||
+            testsWin.length    ||
+            testsMac.length    ||
+            errors.length      ||
+            errorsLinux.length ||
+            errorsWin.length   ||
+            errorsMac.length) {
             QUnit.test(name, assert => {
-                tests.forEach(t => {
-                    let env = undefined
-                    try { env = setup() } catch (e) {}
+                const platform = process.platform
 
-                    const actual = evaluate(t.test)
-                    if (typeof(t.result) === 'function') {
-                        assert.ok(t.result(actual, env), t.test)
-                    } else {
-                        assert.deepEqual(actual, t.result, t.test)
-                    }
+                // Run generic tests
+                tests.forEach(t => TestRunner.runTest(assert, setup, teardown, t))
 
-                    try { teardown(env) } catch (e) {}
-                })
+                // Run platform specific tests
+                switch (platform) {
+                case 'linux':
+                    testsLinux.forEach(t => TestRunner.runTest(assert, setup, teardown, t))
+                    break
+                case 'win32':
+                    testsWin.forEach(t => TestRunner.runTest(assert, setup, teardown, t))
+                    break
+                case 'darwin':
+                    testsMac.forEach(t => TestRunner.runTest(assert, setup, teardown, t))
+                    break
+                default:
+                    throw new Error(`Unknown platform: ${platform}`)
+                }
 
-                errors.forEach(t => {
-                    let env = undefined
-                    try { env = setup() } catch (e) {}
+                // Run generic tests
+                errors.forEach(t => TestRunner.runError(assert, setup, teardown, t))
 
-                    assert.throws(() => evaluate(t.test), t.result, t.test)
-
-                    try { teardown(env) } catch (e) {}
-                })
+                // Run platform specific tests
+                switch (platform) {
+                case 'linux':
+                    errorsLinux.forEach(t => TestRunner.runTest(assert, setup, teardown, t))
+                    break
+                case 'win32':
+                    errorsWin.forEach(t => TestRunner.runTest(assert, setup, teardown, t))
+                    break
+                case 'darwin':
+                    errorsMac.forEach(t => TestRunner.runTest(assert, setup, teardown, t))
+                    break
+                default:
+                    throw new Error(`Unknown platform: ${platform}`)
+                }
             })
         }
+    }
+
+    static runTest(assert, setup, teardown, t) {
+        let env = undefined
+        try { env = setup() } catch (e) {}
+
+        const actual = evaluate(t.test)
+        if (typeof(t.result) === 'function') {
+            assert.ok(t.result(actual, env), t.test)
+        } else {
+            assert.deepEqual(actual, t.result, t.test)
+        }
+
+        try { teardown(env) } catch (e) {}
+    }
+
+    static runError(assert, setup, teardown, t) {
+        let env = undefined
+        try { env = setup() } catch (e) {}
+
+        assert.throws(() => evaluate(t.test), t.result, t.test)
+
+        try { teardown(env) } catch (e) {}
     }
 }
 
