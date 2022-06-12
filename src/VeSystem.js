c8748485da673c2237738fb535d48b72d211d39a
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-06-12 21:36:28 +0300

    Fix name

--- src/VeSystem.js
+++ src/VeSystem.js
@@ -5,7 +5,7 @@ const tmpDir = () => {
     const tmp = process.env['TMP']
     if (tmp && fs.existsSync(tmp)) { return tmp }
     const temp = process.env['TEMP']
-    if (tmp && fs.existsSync(temp)) { return temp }
+    if (temp && fs.existsSync(temp)) { return temp }
     return os.tmpdir()
 }
 
