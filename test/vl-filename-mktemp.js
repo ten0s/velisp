cd9036867bc9dd6da3c18271146010b54bde7eae
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-06-12 22:54:40 +0300

    Review vl-filename-mktemp on Windows

--- test/vl-filename-mktemp.js
+++ test/vl-filename-mktemp.js
@@ -1,83 +1,75 @@
-import os from 'os'
+import path from 'path'
 import {TestRunner} from './test-runner.js'
 import {Str} from '../src/VeLispTypes.js'
+import {homeDir, tmpDir} from '../src/VeSystem.js'
 
 TestRunner.run({
     name: 'vl-filename-mktemp',
 
-    setup: () => {
-        const oldtmp = process.env['TMP']
-        const oldtemp = process.env['TEMP']
-        delete process.env['TMP']
-        delete process.env['TEMP']
-        return {oldtmp, oldtemp}
-    },
-
-    teardown: ({oldtmp, oldtemp}) => {
-        process.env['TMP'] = oldtmp
-        process.env['TEMP'] = oldtemp
-    },
-
     tests: [
         // Test `pattern`
-        {test: '(setenv "TMP" (tmpdir)) (vl-filename-mktemp)', result: (act) => {
+        {test: '(vl-filename-mktemp)', result: (act) => {
             //console.log(act)
             return act instanceof Str
-                && act.value().startsWith(`${os.tmpdir()}/velisp-`)
+                && act.value().startsWith(`${tmpDir()}${path.sep}velisp-`)
         }},
-        {test: '(setenv "TMP" (tmpdir)) (vl-filename-mktemp "")', result: (act) => {
+
+        {test: '(vl-filename-mktemp "")', result: (act) => {
             //console.log(act)
             return act instanceof Str
-                && act.value().startsWith(`${os.tmpdir()}/velisp-`)
+                && act.value().startsWith(`${tmpDir()}${path.sep}velisp-`)
         }},
-        {test: '(setenv "TMP" (tmpdir)) (vl-filename-mktemp nil)', result: (act) => {
+
+        {test: '(vl-filename-mktemp "prefix--")', result: (act) => {
             //console.log(act)
             return act instanceof Str
-                && act.value().startsWith(`${os.tmpdir()}/velisp-`)
+                && act.value().startsWith(`${tmpDir()}${path.sep}prefix--`)
         }},
-        {test: '(setenv "TMP" (tmpdir)) (vl-filename-mktemp T)', result: (act) => {
+
+        {test: '(vl-filename-mktemp nil)', result: (act) => {
             //console.log(act)
             return act instanceof Str
-                && act.value().startsWith(`${os.tmpdir()}/velisp-`)
+                && act.value().startsWith(`${tmpDir()}${path.sep}velisp-`)
         }},
-        {test: '(setenv "TMP" (tmpdir)) (vl-filename-mktemp nil nil nil)', result: (act) => {
+
+        {test: '(vl-filename-mktemp T)', result: (act) => {
             //console.log(act)
             return act instanceof Str
-                && act.value().startsWith(`${os.tmpdir()}/velisp-`)
+                && act.value().startsWith(`${tmpDir()}${path.sep}velisp-`)
         }},
-        {test: '(setenv "TMP" (tmpdir)) (vl-filename-mktemp T T T)', result: (act) => {
+
+        {test: '(vl-filename-mktemp nil nil nil)', result: (act) => {
             //console.log(act)
             return act instanceof Str
-                && act.value().startsWith(`${os.tmpdir()}/velisp-`)
+                && act.value().startsWith(`${tmpDir()}${path.sep}velisp-`)
         }},
-        {test: '(setenv "TEMP" (tmpdir)) (vl-filename-mktemp)', result: (act) => {
+
+        {test: '(vl-filename-mktemp T T T)', result: (act) => {
             //console.log(act)
             return act instanceof Str
-                && act.value().startsWith(`${os.tmpdir()}/velisp-`)
-        }},
-        {test: '(setenv "TMP" "") (setenv "TEMP" "") (vl-filename-mktemp)', result: (act) => {
-            //console.log(act)
-            return act instanceof Str
-                && act.value().startsWith(`${os.tmpdir()}/velisp-`)
-        }},
-        {test: '(setenv "TMP" ".") (setenv "TEMP" ".") (vl-filename-mktemp)', result: (act) => {
-            //console.log(act)
-            return act instanceof Str
-                && act.value().startsWith('velisp-')
+                && act.value().startsWith(`${tmpDir()}${path.sep}velisp-`)
         }},
 
         // Test `directory`
-        {test: '(setenv "TMP" (tmpdir)) (vl-filename-mktemp nil (homedir))', result: (act) => {
+        {test: '(vl-filename-mktemp nil (homedir))', result: (act) => {
             //console.log(act)
             return act instanceof Str
-                && act.value().startsWith(`${os.homedir()}/velisp-`)
+                && act.value().startsWith(`${homeDir()}${path.sep}velisp-`)
         }},
 
         // Test `extension`
-        {test: '(vl-filename-mktemp nil nil ".text")', result: (act) => {
+        {test: '(vl-filename-mktemp nil nil ".suffix")', result: (act) => {
             //console.log(act)
             return act instanceof Str
-                && act.value().endsWith('.text')
+                && act.value().endsWith('.suffix')
+        }},
+
+        // Test all args
+        {test: '(vl-filename-mktemp "prefix--" (homedir) ".suffix")', result: (act) => {
+            //console.log(act)
+            return act instanceof Str
+                && act.value().startsWith(`${homeDir()}${path.sep}prefix--`)
+                && act.value().endsWith('.suffix')
         }},
     ],
 
