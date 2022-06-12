c6acf3bbc31f77b34215f351b984fc10d05a03e2
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-06-12 21:34:45 +0300

    Review tmpdir test

--- test/tmpdir.js
+++ test/tmpdir.js
@@ -1,12 +1,26 @@
 import os from 'os'
 import {TestRunner} from './test-runner.js'
 import {Str} from '../src/VeLispTypes.js'
+import {tmpDir} from '../src/VeSystem.js'
 
 TestRunner.run({
     name: 'tmpdir',
 
+    setup: () => {
+        const oldtmp = process.env['TMP']
+        const oldtemp = process.env['TEMP']
+        return {oldtmp, oldtemp}
+    },
+
+    teardown: ({oldtmp, oldtemp}) => {
+        process.env['TMP'] = oldtmp
+        process.env['TEMP'] = oldtemp
+    },
+
     tests: [
-        {test: '(tmpdir)', result: new Str(os.tmpdir())},
+        {test: '(setenv "TMP" ".") (setenv "TEMP" "") (tmpdir)', result: new Str('.')},
+        {test: '(setenv "TMP" "") (setenv "TEMP" ".") (tmpdir)', result: new Str('.')},
+        {test: '(tmpdir)', result: new Str(tmpDir())},
     ],
 
     errors: [
