08fba0de7b3bf60c1ac576f59779c70670790a13
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-06-13 16:36:39 +0300

    Review read-write-{char,line} tests

--- test/read-write-char.js
+++ test/read-write-char.js
@@ -1,17 +1,31 @@
-import QUnit from 'qunit'
 import fs from 'fs'
+import {TestRunner} from './test-runner.js'
 import {evaluate} from '../src/VeLispEvaluator.js'
 import {Bool, Str} from '../src/VeLispTypes.js'
 
-const tests = [
-    {test:`
+const touch = (file) => fs.closeSync(fs.openSync(file, 'w'))
+const rm    = (file) => fs.rmSync(file, {force: true})
+
+TestRunner.run({
+    name: 'read-write-char',
+
+    setup: () => {
+        touch('file.txt')
+    },
+
+    teardown: () => {
+        rm('file.txt')
+    },
+
+    tests: [
+        {test:`
 (setq f (open "file.txt" "r"))
 (setq res (read-char f))
 (close f)
 res
 `, result: new Bool(false)},
 
-    {test: `
+        {test: `
 (setq f (open "file.txt" "w"))
 (write-char (ascii "H") f)
 (write-char (ascii "e") f)
@@ -28,49 +42,34 @@ res
 (close f)
 (strcat c1 c2 c3 c4 c5)
 `, result: new Str('Hello')},
-]
+    ],
 
-const errors = [
-    // Disabled since it reads from stdin
-    //{test: '(read-char)', result:
-    // new Error('read-char: too few arguments')},
-    {test: '(setq f (open "file.txt" "r")) (read-char f f)', result:
-     new Error('read-char: too many arguments')},
-    {test: '(read-char 0)', result:
-     new Error('read-char: `file-desc` expected File')},
-    {test: '(setq f (open "file.txt" "r")) (close f) (read-char f)', result:
-     new Error('read-char: bad file #<file "file.txt" r:c>')},
-    {test: '(setq f (open "file.txt" "w")) (read-char f)', result:
-     new Error('read-char: bad file #<file "file.txt" w:o>')},
-    {test: '(setq f (open "file.txt" "a")) (read-char f)', result:
-     new Error('read-char: bad file #<file "file.txt" a:o>')},
+    errors: [
+        // Disabled since it reads from stdin
+        //{test: '(read-char)', result:
+        // new Error('read-char: too few arguments')},
+        {test: '(setq f (open "file.txt" "r")) (read-char f f)', result:
+         new Error('read-char: too many arguments')},
+        {test: '(read-char 0)', result:
+         new Error('read-char: `file-desc` expected File')},
+        {test: '(setq f (open "file.txt" "r")) (close f) (read-char f)', result:
+         new Error('read-char: bad file #<file "file.txt" r:c>')},
+        {test: '(setq f (open "file.txt" "w")) (read-char f)', result:
+         new Error('read-char: bad file #<file "file.txt" w:o>')},
+        {test: '(setq f (open "file.txt" "a")) (read-char f)', result:
+         new Error('read-char: bad file #<file "file.txt" a:o>')},
 
-    {test: '(write-char)', result:
-     new Error('write-char: too few arguments')},
-    {test: '(setq f (open "file.txt" "w")) (write-char 65 f f)', result:
-     new Error('write-char: too many arguments')},
-    {test: '(write-char "A" 1)', result:
-     new Error('write-char: `num` expected Int')},
-    {test: '(write-char 65  1)', result:
-     new Error('write-char: `file-desc` expected File')},
-    {test: '(setq f (open "file.txt" "w")) (close f) (write-char 65 f)', result:
-     new Error('write-char: bad file #<file "file.txt" w:c>')},
-    {test: '(setq f (open "file.txt" "r")) (write-char 65 f)', result:
-     new Error('write-char: bad file #<file "file.txt" r:o>')},
-]
-
-QUnit.test('read-write-char', assert => {
-    // Setup
-    fs.closeSync(fs.openSync('file.txt', 'w'))
-
-    tests.forEach(t => {
-        assert.deepEqual(evaluate(t.test), t.result, t.test)
-    })
-
-    errors.forEach(t => {
-        assert.throws(() => evaluate(t.test), t.result, t.test)
-    })
-
-    // Tear down
-    fs.unlinkSync('file.txt')
+        {test: '(write-char)', result:
+         new Error('write-char: too few arguments')},
+        {test: '(setq f (open "file.txt" "w")) (write-char 65 f f)', result:
+         new Error('write-char: too many arguments')},
+        {test: '(write-char "A" 1)', result:
+         new Error('write-char: `num` expected Int')},
+        {test: '(write-char 65  1)', result:
+         new Error('write-char: `file-desc` expected File')},
+        {test: '(setq f (open "file.txt" "w")) (close f) (write-char 65 f)', result:
+         new Error('write-char: bad file #<file "file.txt" w:c>')},
+        {test: '(setq f (open "file.txt" "r")) (write-char 65 f)', result:
+         new Error('write-char: bad file #<file "file.txt" r:o>')},
+    ]
 })
--- test/read-write-line.js
+++ test/read-write-line.js
@@ -1,17 +1,31 @@
-import QUnit from 'qunit'
 import fs from 'fs'
+import {TestRunner} from './test-runner.js'
 import {evaluate} from '../src/VeLispEvaluator.js'
 import {Bool, Str} from '../src/VeLispTypes.js'
 
-const tests = [
-    {test:`
+const touch = (file) => fs.closeSync(fs.openSync(file, 'w'))
+const rm    = (file) => fs.rmSync(file, {force: true})
+
+TestRunner.run({
+    name: 'read-write-line',
+
+    setup: () => {
+        touch('file.txt')
+    },
+
+    teardown: () => {
+        rm('file.txt')
+    },
+
+    tests: [
+        {test:`
 (setq f (open "file.txt" "r"))
 (setq res (read-line f))
 (close f)
 res
 `, result: new Bool(false)},
 
-    {test: `
+        {test: `
 (setq f (open "file.txt" "w"))
 (write-line "Hello" f)
 (write-line " " f)
@@ -24,49 +38,34 @@ res
 (close f)
 (strcat l1 l2 l3)
 `, result: new Str('Hello World')},
-]
+    ],
 
-const errors = [
-    // Disabled since it reads from stdin
-    //{test: '(read-line)', result:
-    // new Error('read-line: too few arguments')},
-    {test: '(setq f (open "file.txt" "r")) (read-line f f)', result:
-     new Error('read-line: too many arguments')},
-    {test: '(read-line 0)', result:
-     new Error('read-line: `file-desc` expected File')},
-    {test: '(setq f (open "file.txt" "r")) (close f) (read-line f)', result:
-     new Error('read-line: bad file #<file "file.txt" r:c>')},
-    {test: '(setq f (open "file.txt" "w")) (read-line f)', result:
-     new Error('read-line: bad file #<file "file.txt" w:o>')},
-    {test: '(setq f (open "file.txt" "a")) (read-line f)', result:
-     new Error('read-line: bad file #<file "file.txt" a:o>')},
+    errors: [
+        // Disabled since it reads from stdin
+        //{test: '(read-line)', result:
+        // new Error('read-line: too few arguments')},
+        {test: '(setq f (open "file.txt" "r")) (read-line f f)', result:
+         new Error('read-line: too many arguments')},
+        {test: '(read-line 0)', result:
+         new Error('read-line: `file-desc` expected File')},
+        {test: '(setq f (open "file.txt" "r")) (close f) (read-line f)', result:
+         new Error('read-line: bad file #<file "file.txt" r:c>')},
+        {test: '(setq f (open "file.txt" "w")) (read-line f)', result:
+         new Error('read-line: bad file #<file "file.txt" w:o>')},
+        {test: '(setq f (open "file.txt" "a")) (read-line f)', result:
+         new Error('read-line: bad file #<file "file.txt" a:o>')},
 
-    {test: '(write-line)', result:
-     new Error('write-line: too few arguments')},
-    {test: '(setq f (open "file.txt" "w")) (write-line "str" f f)', result:
-     new Error('write-line: too many arguments')},
-    {test: '(write-line 65 1)', result:
-     new Error('write-line: `string` expected Str')},
-    {test: '(write-line "str" 1)', result:
-     new Error('write-line: `file-desc` expected File')},
-    {test: '(setq f (open "file.txt" "w")) (close f) (write-line "str" f)', result:
-     new Error('write-line: bad file #<file "file.txt" w:c>')},
-    {test: '(setq f (open "file.txt" "r")) (write-line "str" f)', result:
-     new Error('write-line: bad file #<file "file.txt" r:o>')},
-]
-
-QUnit.test('read-write-line', assert => {
-    // Setup
-    fs.closeSync(fs.openSync('file.txt', 'w'))
-
-    tests.forEach(t => {
-        assert.deepEqual(evaluate(t.test), t.result, t.test)
-    })
-
-    errors.forEach(t => {
-        assert.throws(() => evaluate(t.test), t.result, t.test)
-    })
-
-    // Tear down
-    fs.unlinkSync('file.txt')
+        {test: '(write-line)', result:
+         new Error('write-line: too few arguments')},
+        {test: '(setq f (open "file.txt" "w")) (write-line "str" f f)', result:
+         new Error('write-line: too many arguments')},
+        {test: '(write-line 65 1)', result:
+         new Error('write-line: `string` expected Str')},
+        {test: '(write-line "str" 1)', result:
+         new Error('write-line: `file-desc` expected File')},
+        {test: '(setq f (open "file.txt" "w")) (close f) (write-line "str" f)', result:
+         new Error('write-line: bad file #<file "file.txt" w:c>')},
+        {test: '(setq f (open "file.txt" "r")) (write-line "str" f)', result:
+         new Error('write-line: bad file #<file "file.txt" r:o>')},
+    ]
 })
