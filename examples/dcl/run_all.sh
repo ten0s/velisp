d9a7919a96357de9f203a9072a09f0ca785eb115
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-06-16 16:16:44 +0300

    Show running dlg

--- examples/dcl/run_all.sh
+++ examples/dcl/run_all.sh
@@ -4,5 +4,7 @@ for file in `ls ${0%/*}/*.dcl`; do
     dir=$(dirname $file)
     base=$(basename $file)
     name=${base%%.dcl}
+    echo Running ${name}...
     ${dir}/run_dlg.sh ${name}
+    echo
 done
