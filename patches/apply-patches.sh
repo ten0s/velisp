d6b8eb9109f1d4980a6febd86828fde2e82fadbb
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-06-20 17:00:12 +0300

    Update node-gtk branch and a patch

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -5,3 +5,13 @@ DIR=$(dirname $0)
 if [[ ! -f ${DIR}/../node_modules/node-gtk/lib/overrides/Gtk-3.0.js.orig ]]; then
     patch --backup ${DIR}/../node_modules/node-gtk/lib/overrides/Gtk-3.0.js ${DIR}/Gtk-3.0.js.patch
 fi
+
+if [[ `uname -s` =~ "MINGW64" ]]; then
+
+# Don't free array memory. On Windows there are unknown crashes.
+# Needs to be investigated.
+if [[ ! -f ${DIR}/../node_modules/node-gtk/src/value.cc.orig ]]; then
+    patch --backup ${DIR}/../node_modules/node-gtk/src/value.cc ${DIR}/value.cc.patch
+fi
+
+fi
