f93b3911ff181b0d22ad3eec7190a1b33cc40479
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-07-18 19:21:13 +0300

    Upgrade node-gtk
    Removes upstream applied patch

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -2,6 +2,4 @@
 
 DIR=$(dirname $0)
 
-if [[ ! -f ${DIR}/../node_modules/node-gtk/lib/overrides/Gtk-3.0.js.orig ]]; then
-    patch --backup ${DIR}/../node_modules/node-gtk/lib/overrides/Gtk-3.0.js ${DIR}/Gtk-3.0.js.patch
-fi
+# Apply patches here
