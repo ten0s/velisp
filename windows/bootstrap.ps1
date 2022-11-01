2288f9ff8c593e8808b285a31feec3c87650a981
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-11-01 13:03:26 +0300

    Remove unused SSH from Windows build machine

--- windows/bootstrap.ps1
+++ windows/bootstrap.ps1
@@ -1,11 +1,3 @@
-# Enable SSH Server
-Get-WindowsCapability -Online | ? Name -like "OpenSSH*"
-Add-WindowsCapability -Online -Name "OpenSSH.Server~~~~0.0.1.0"
-New-NetFirewallRule -Protocol TCP -LocalPort 22 -Direction Inbound -Action Allow -DisplayName ssh
-Get-Service | ? Name -like '*ssh*'
-Start-Service -Name sshd
-Set-Service -Name sshd -StartupType Automatic
-
 # Install Choco
 #Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
@@ -24,8 +16,3 @@ npm config set msvs_version 2017
 choco install -y msys2 --params "/InstallDir:C:\\msys64"
 C:\\msys64\\msys2_shell.cmd -defterm -no-start -mingw64 -c "pacman --noconfirm -S base-devel expect git mingw-w64-x86_64-jq mingw-w64-x86_64-gtk3 mingw-w64-x86_64-gobject-introspection mingw-w64-x86_64-cairo zip"
 [Environment]::SetEnvironmentVariable('PATH', [Environment]::GetEnvironmentVariable('PATH', 'User') + ';C:\\msys64\\mingw64\\bin', 'User')
-
-# Make Mingw64 SSH default shell https://www.msys2.org/wiki/Setting-up-SSHd/
-'@set HOME=' | Out-File -FilePath C:\\msys64\\sshd_default_shell.cmd -Encoding ASCII
-'@C:\\msys64\\msys2_shell.cmd -defterm -full-path -no-start -mingw64' | Out-File -FilePath C:\\msys64\\sshd_default_shell.cmd -Encoding ASCII -Append
-New-ItemProperty -Path "HKLM:\\SOFTWARE\\OpenSSH" -Name DefaultShell -Value 'C:\\msys64\\sshd_default_shell.cmd' -PropertyType String -Force
