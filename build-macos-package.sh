6c7f449606a0cf45657fa961d2cb3ef9c86262d6
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-11-15 14:40:11 +0300

    Rsync tar.gz from MacOS guest
    Shared folder type: rsync is one-way only

--- build-macos-package.sh
+++ build-macos-package.sh
@@ -10,8 +10,21 @@ fi
 
 vagrant ssh -c 'source ~/.zshrc; cd velisp; macos/build.sh'
 if [[ $? -eq 0 ]]; then
-    echo "Build Done"
+    vagrant ssh-config | awk '
+        /HostName /     { host = $2 }
+        /Port /         { port = $2 }
+        /User /         { user = $2 }
+        /IdentityFile / { id   = $2 }
+        END             { printf("rsync -vv -e \"ssh -p%s -i%s\" %s@%s:velisp/velisp-*-*-*.tar.gz .\n", port, id, user, host) }
+    ' | bash
+    if [[ $? -eq 0 ]]; then
+        echo "Build Done"
+    else
+        echo "Copy Failed"
+        exit 2
+    fi
+
 else
     echo "Build Failed"
-    exit 2
+    exit 3
 fi
