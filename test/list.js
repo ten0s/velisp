d513de2d2dc684184a025ea359202d6c737e1b77
Dmitry Klionsky <dm.klionsky@gmail.com>
2023-01-05 14:13:08 +0300

    Refactor tests

--- test/list.js
+++ test/list.js
@@ -1,67 +1,60 @@
-import QUnit from 'qunit'
-import {evaluate} from '../src/VeLispEvaluator.js'
+import {TestRunner} from './test-runner.js'
 import {Bool, Int, Real, Str, List, Pair} from '../src/VeLispTypes.js'
 
-const tests = [
-    {test: '(list)', result: new List([])},
-    {test: '(list 1 2 3)', result: new List([
-        new Int(1), new Int(2), new Int(3)
-    ])},
-    {test: '(list 1 "2" 3.0 (list 4))', result: new List([
-        new Int(1), new Str('2'), new Real(3.0), new List([new Int(4)])
-    ])},
+TestRunner.run({
+    name: 'list',
 
-    {test: '(cons 1 (list))', result: new List([new Int(1)])},
-    {test: '(cons 1 nil)', result: new List([new Int(1)])},
-    {test: '(cons 1 (list 2 3))', result: new List([
-        new Int(1), new Int(2), new Int(3)
-    ])},
-    {test: '(cons 1 (cons 2 (cons 3 nil)))', result: new List([
-        new Int(1), new Int(2), new Int(3)
-    ])},
-    {test: '(cons (list 1) (list 2 3))', result: new List([
-        new List([new Int(1)]), new Int(2), new Int(3)
-    ])},
+    tests: [
+        {test: '(list)', result: new List([])},
+        {test: '(list 1 2 3)', result: new List([
+            new Int(1), new Int(2), new Int(3)
+        ])},
+        {test: '(list 1 "2" 3.0 (list 4))', result: new List([
+            new Int(1), new Str('2'), new Real(3.0), new List([new Int(4)])
+        ])},
 
-    {test: '(cons 1 (cons 2 3))', result: new List([
-        new Int(1), new Pair(new Int(2), new Int(3))
-    ])},
+        {test: '(cons 1 (list))', result: new List([new Int(1)])},
+        {test: '(cons 1 nil)', result: new List([new Int(1)])},
+        {test: '(cons 1 (list 2 3))', result: new List([
+            new Int(1), new Int(2), new Int(3)
+        ])},
+        {test: '(cons 1 (cons 2 (cons 3 nil)))', result: new List([
+            new Int(1), new Int(2), new Int(3)
+        ])},
+        {test: '(cons (list 1) (list 2 3))', result: new List([
+            new List([new Int(1)]), new Int(2), new Int(3)
+        ])},
 
-    {test: '(car (list 1 2 3))', result: new Int(1)},
-    {test: '(car (cons 1 2))', result: new Int(1)},
-    {test: '(car (list))', result: new Bool(false)},
-    {test: '(car nil)', result: new Bool(false)},
-    {test: '(car ())', result: new Bool(false)},
+        {test: '(cons 1 (cons 2 3))', result: new List([
+            new Int(1), new Pair(new Int(2), new Int(3))
+        ])},
 
-    {test: '(cdr (list 1 2 3))', result: new List([
-        new Int(2), new Int(3)
-    ])},
-    {test: '(cdr (cons 1 2))', result: new Int(2)},
-    {test: '(cdr (list))', result: new Bool(false)},
-    {test: '(cdr nil)', result: new Bool(false)},
-    {test: '(cdr ())', result: new Bool(false)},
-]
+        {test: '(car (list 1 2 3))', result: new Int(1)},
+        {test: '(car (cons 1 2))', result: new Int(1)},
+        {test: '(car (list))', result: new Bool(false)},
+        {test: '(car nil)', result: new Bool(false)},
+        {test: '(car ())', result: new Bool(false)},
 
-const errors = [
-    {test: '(cons)', result: new Error('cons: too few arguments')},
-    {test: '(cons 1)', result: new Error('cons: too few arguments')},
-    {test: '(cons 1 2 3)', result: new Error('cons: too many arguments')},
+        {test: '(cdr (list 1 2 3))', result: new List([
+            new Int(2), new Int(3)
+        ])},
+        {test: '(cdr (cons 1 2))', result: new Int(2)},
+        {test: '(cdr (list))', result: new Bool(false)},
+        {test: '(cdr nil)', result: new Bool(false)},
+        {test: '(cdr ())', result: new Bool(false)},
+    ],
 
-    {test: '(car)', result: new Error('car: too few arguments')},
-    {test: '(car (list 1 2) (list 3))', result: new Error('car: too many arguments')},
-    {test: '(car T)', result: new Error('car: expected List, Pair')},
+    errors: [
+        {test: '(cons)', result: new Error('cons: too few arguments')},
+        {test: '(cons 1)', result: new Error('cons: too few arguments')},
+        {test: '(cons 1 2 3)', result: new Error('cons: too many arguments')},
 
-    {test: '(cdr)', result: new Error('cdr: too few arguments')},
-    {test: '(cdr (list 1 2) (list 3))', result: new Error('cdr: too many arguments')},
-    {test: '(cdr T)', result: new Error('cdr: expected List, Pair')},
-]
+        {test: '(car)', result: new Error('car: too few arguments')},
+        {test: '(car (list 1 2) (list 3))', result: new Error('car: too many arguments')},
+        {test: '(car T)', result: new Error('car: expected List, Pair')},
 
-QUnit.test('list', assert => {
-    tests.forEach(t => {
-        assert.deepEqual(evaluate(t.test), t.result, t.test)
-    })
-
-    errors.forEach(t => {
-        assert.throws(() => evaluate(t.test), t.result, t.test)
-    })
+        {test: '(cdr)', result: new Error('cdr: too few arguments')},
+        {test: '(cdr (list 1 2) (list 3))', result: new Error('cdr: too many arguments')},
+        {test: '(cdr T)', result: new Error('cdr: expected List, Pair')},
+    ]
 })
--- test/quote.js
+++ test/quote.js
@@ -1,91 +1,84 @@
-import QUnit from 'qunit'
-import {evaluate} from '../src/VeLispEvaluator.js'
+import {TestRunner} from './test-runner.js'
 import {Bool, Int, Real, Str, Sym, List, Pair} from '../src/VeLispTypes.js'
 
-const tests = [
-    {test: '(quote nil)', result: new Bool(false)},
-    {test: '\'nil', result: new Bool(false)},
+TestRunner.run({
+    name: 'quote',
 
-    {test: '(quote T)', result: new Sym('t')},
-    {test: '\'T', result: new Sym('t')},
+    tests: [
+        {test: '(quote nil)', result: new Bool(false)},
+        {test: '\'nil', result: new Bool(false)},
 
-    {test: '(quote 1)', result: new Int(1)},
-    {test: '\'1', result: new Int(1)},
+        {test: '(quote T)', result: new Sym('t')},
+        {test: '\'T', result: new Sym('t')},
 
-    {test: '(quote 1.0)', result: new Real(1.0)},
-    {test: '\'1.0', result: new Real(1.0)},
+        {test: '(quote 1)', result: new Int(1)},
+        {test: '\'1', result: new Int(1)},
 
-    {test: '(quote "1")', result: new Str('1')},
-    {test: '\'"1"', result: new Str('1')},
+        {test: '(quote 1.0)', result: new Real(1.0)},
+        {test: '\'1.0', result: new Real(1.0)},
 
-    {test: '(quote foo)', result: new Sym('foo')},
-    {test: '\'foo', result: new Sym('foo')},
+        {test: '(quote "1")', result: new Str('1')},
+        {test: '\'"1"', result: new Str('1')},
 
-    {test: '(quote ())', result: new List([])},
-    {test: '\'()', result: new List([])},
+        {test: '(quote foo)', result: new Sym('foo')},
+        {test: '\'foo', result: new Sym('foo')},
 
-    {test: '(quote (1))', result: new List([new Int(1)])},
-    {test: '\'(1)', result: new List([new Int(1)])},
+        {test: '(quote ())', result: new List([])},
+        {test: '\'()', result: new List([])},
 
-    {test: '(quote (nil T 1 1.0 "1" foo))', result: new List([
-        new Bool(false), new Sym('t'), new Int(1), new Real(1.0), new Str('1'), new Sym('foo')
-    ])},
-    {test: '\'(nil T 1 1.0 "1" foo)', result: new List([
-        new Bool(false), new Sym('t'), new Int(1), new Real(1.0), new Str('1'), new Sym('foo')
-    ])},
+        {test: '(quote (1))', result: new List([new Int(1)])},
+        {test: '\'(1)', result: new List([new Int(1)])},
 
-    {test: '(quote (list))', result: new List([new Sym('list')])},
-    {test: '\'(list)', result: new List([new Sym('list')])},
+        {test: '(quote (nil T 1 1.0 "1" foo))', result: new List([
+            new Bool(false), new Sym('t'), new Int(1), new Real(1.0), new Str('1'), new Sym('foo')
+        ])},
+        {test: '\'(nil T 1 1.0 "1" foo)', result: new List([
+            new Bool(false), new Sym('t'), new Int(1), new Real(1.0), new Str('1'), new Sym('foo')
+        ])},
 
-    {test: '(quote (list 1 2 3))', result: new List([
-        new Sym('list'), new Int(1), new Int(2), new Int(3)
-    ])},
-    {test: '\'(list 1 2 3)', result: new List([
-        new Sym('list'), new Int(1), new Int(2), new Int(3)
-    ])},
+        {test: '(quote (list))', result: new List([new Sym('list')])},
+        {test: '\'(list)', result: new List([new Sym('list')])},
 
-    {test: '(quote (1 (2) ((3))))', result: new List([
-        new Int(1),
-        new List([new Int(2)]),
-        new List([new List([new Int(3)])])
-    ])},
+        {test: '(quote (list 1 2 3))', result: new List([
+            new Sym('list'), new Int(1), new Int(2), new Int(3)
+        ])},
+        {test: '\'(list 1 2 3)', result: new List([
+            new Sym('list'), new Int(1), new Int(2), new Int(3)
+        ])},
 
-    {test: '(quote (a . b))', result: new Pair(new Sym('a'), new Sym('b'))},
-    {test: '\'(a . b))', result: new Pair(new Sym('a'), new Sym('b'))},
+        {test: '(quote (1 (2) ((3))))', result: new List([
+            new Int(1),
+            new List([new Int(2)]),
+            new List([new List([new Int(3)])])
+        ])},
 
-    {test: '(quote (1 2 . z))', result: new List([
-        new Int(1), new Pair(new Int(2), new Sym('z'))
-    ])},
-    {test: '\'(1 2 . z)', result: new List([
-        new Int(1), new Pair(new Int(2), new Sym('z'))
-    ])},
+        {test: '(quote (a . b))', result: new Pair(new Sym('a'), new Sym('b'))},
+        {test: '\'(a . b))', result: new Pair(new Sym('a'), new Sym('b'))},
 
-    {test: '(apply (quote (lambda (x) x)) (quote (z)))', result: new Sym('z')},
-    {test: '(apply \'(lambda (x) x) \'(z))', result: new Sym('z')},
+        {test: '(quote (1 2 . z))', result: new List([
+            new Int(1), new Pair(new Int(2), new Sym('z'))
+        ])},
+        {test: '\'(1 2 . z)', result: new List([
+            new Int(1), new Pair(new Int(2), new Sym('z'))
+        ])},
 
-    {test: '(quote (quote foo))', result: new List([new Sym('quote'), new Sym('foo')])},
-    {test: '\'(quote foo)', result: new List([new Sym('quote'), new Sym('foo')])},
-]
+        {test: '(apply (quote (lambda (x) x)) (quote (z)))', result: new Sym('z')},
+        {test: '(apply \'(lambda (x) x) \'(z))', result: new Sym('z')},
 
-const errors = [
-    {test: '\'(and)', result: new Error('quote: `and` not supported')},
-    {test: '\'(cond)', result: new Error('quote: `cond` not supported')},
-    {test: '\'(defun id (x) x)', result: new Error('quote: `defun` not supported')},
-    {test: '\'(foreach x \'())', result: new Error('quote: `foreach` not supported')},
-    {test: '\'(if T T)', result: new Error('quote: `if` not supported')},
-    {test: '\'(or)', result: new Error('quote: `or` not supported')},
-    {test: '\'(progn)', result: new Error('quote: `progn` not supported')},
-    {test: '\'(repeat 1)', result: new Error('quote: `repeat` not supported')},
-    {test: '\'(setq)', result: new Error('quote: `setq` not supported')},
-    {test: '\'(while nil nil)', result: new Error('quote: `while` not supported')},
-]
+        {test: '(quote (quote foo))', result: new List([new Sym('quote'), new Sym('foo')])},
+        {test: '\'(quote foo)', result: new List([new Sym('quote'), new Sym('foo')])},
+    ],
 
-QUnit.test('quote', assert => {
-    tests.forEach(t => {
-        assert.deepEqual(evaluate(t.test), t.result, t.test)
-    })
-
-    errors.forEach(t => {
-        assert.throws(() => evaluate(t.test), t.result, t.test)
-    })
+    errors: [
+        {test: '\'(and)', result: new Error('quote: `and` not supported')},
+        {test: '\'(cond)', result: new Error('quote: `cond` not supported')},
+        {test: '\'(defun id (x) x)', result: new Error('quote: `defun` not supported')},
+        {test: '\'(foreach x \'())', result: new Error('quote: `foreach` not supported')},
+        {test: '\'(if T T)', result: new Error('quote: `if` not supported')},
+        {test: '\'(or)', result: new Error('quote: `or` not supported')},
+        {test: '\'(progn)', result: new Error('quote: `progn` not supported')},
+        {test: '\'(repeat 1)', result: new Error('quote: `repeat` not supported')},
+        {test: '\'(setq)', result: new Error('quote: `setq` not supported')},
+        {test: '\'(while nil nil)', result: new Error('quote: `while` not supported')},
+    ]
 })
