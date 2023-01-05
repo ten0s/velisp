08a127e067be508cd769bf54abeedac8aea62c53
Dmitry Klionsky <dm.klionsky@gmail.com>
2023-01-05 12:30:15 +0300

    Refactor test

--- test/append.js
+++ test/append.js
@@ -1,33 +1,26 @@
-import QUnit from 'qunit'
-import {evaluate} from '../src/VeLispEvaluator.js'
+import {TestRunner} from './test-runner.js'
 import {Bool, Int, List} from '../src/VeLispTypes.js'
 
-const tests = [
-    {test: '(append)', result: new Bool(false)},
-    {test: '(append nil)', result: new List([])},
-    {test: '(append ())', result: new List([])},
-    {test: '(append \'())', result: new List([])},
-    {test: '(append \'(1))', result: new List([new Int(1)])},
-    {test: '(append \'(1) \'(2 3))', result: new List([
-        new Int(1), new Int(2), new Int(3)
-    ])},
-    {test: '(append \'(97) \'(96 44) \'(98) \'(96 44) \'(99))', result: new List([
-        new Int(97), new Int(96), new Int(44), new Int(98),
-        new Int(96), new Int(44), new Int(99)
-    ])},
-]
+TestRunner.run({
+    name: 'append',
 
-const errors = [
-    {test: '(append 1)', result: new Error('append: expected List')},
-    {test: '(append \'(1) 2)', result: new Error('append: expected List')},
-]
+    tests: [
+        {test: '(append)', result: new Bool(false)},
+        {test: '(append nil)', result: new List([])},
+        {test: '(append ())', result: new List([])},
+        {test: '(append \'())', result: new List([])},
+        {test: '(append \'(1))', result: new List([new Int(1)])},
+        {test: '(append \'(1) \'(2 3))', result: new List([
+            new Int(1), new Int(2), new Int(3)
+        ])},
+        {test: '(append \'(97) \'(96 44) \'(98) \'(96 44) \'(99))', result: new List([
+            new Int(97), new Int(96), new Int(44), new Int(98),
+            new Int(96), new Int(44), new Int(99)
+        ])},
+    ],
 
-QUnit.test('append', assert => {
-    tests.forEach(t => {
-        assert.deepEqual(evaluate(t.test), t.result, t.test)
-    })
-
-    errors.forEach(t => {
-        assert.throws(() => evaluate(t.test), t.result, t.test)
-    })
+    errors: [
+        {test: '(append 1)', result: new Error('append: expected List')},
+        {test: '(append \'(1) 2)', result: new Error('append: expected List')},
+    ]
 })
