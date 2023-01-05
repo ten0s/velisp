7fa4b0690a9d8f43b9f4c3408623ec2b5f070415
Dmitry Klionsky <dm.klionsky@gmail.com>
2023-01-05 18:21:29 +0300

    Fix path

--- windows/build.sh
+++ windows/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-curl -s https://raw.githubusercontent.com/ten0s/node-gtk/windows-build/windows/mingw_include_extra.sh | bash
+curl -s https://raw.githubusercontent.com/ten0s/node-gtk/master/windows/mingw_include_extra.sh | bash
 
-export MINGW_WINDOWS_PATH=$(curl -s https://raw.githubusercontent.com/ten0s/node-gtk/windows-build/windows/mingw_windows_path.sh | bash)
+export MINGW_WINDOWS_PATH=$(curl -s https://raw.githubusercontent.com/ten0s/node-gtk/master/windows/mingw_windows_path.sh | bash)
 
 make windowsPackage
 
