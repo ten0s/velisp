c722ca499124dfbbe0186f5985755b4f2c427c77
Dmitry Klionsky <dm.klionsky@gmail.com>
2023-02-26 13:38:55 +0300

    Cleanup

--- test/type.js
+++ test/type.js
@@ -1,33 +1,26 @@
-import QUnit from 'qunit'
-import {evaluate} from '../src/VeLispEvaluator.js'
+import {TestRunner} from './test-runner.js'
 import {Bool, Sym} from '../src/VeLispTypes.js'
 
-const tests = [
-    {test: '(type nil)', result: new Bool(false)},
-    {test: '(type ())', result: new Bool(false)},
-    {test: '(type \'())', result: new Bool(false)},
-    {test: '(type t)', result: new Sym('sym')},
-    {test: '(type 1)', result: new Sym('int')},
-    {test: '(type 1.0)', result: new Sym('real')},
-    {test: '(type "1")', result: new Sym('str')},
-    {test: '(type \'foo)', result: new Sym('sym')},
-    {test: '(type \'(1 2 3))', result: new Sym('list')},
-    {test: '(type \'(1 . 2))', result: new Sym('list')},
-    {test: '(type +)', result: new Sym('subr')},
-    {test: '(type (lambda (x) x))', result: new Sym('usubr')},
-]
+TestRunner.run({
+    name: 'type',
 
-const errors = [
-    {test: '(type)', result: new Error('type: too few arguments')},
-    {test: '(type 1 2)', result: new Error('type: too many arguments')},
-]
+    tests: [
+        {test: '(type nil)', result: new Bool(false)},
+        {test: '(type ())', result: new Bool(false)},
+        {test: '(type \'())', result: new Bool(false)},
+        {test: '(type t)', result: new Sym('sym')},
+        {test: '(type 1)', result: new Sym('int')},
+        {test: '(type 1.0)', result: new Sym('real')},
+        {test: '(type "1")', result: new Sym('str')},
+        {test: '(type \'foo)', result: new Sym('sym')},
+        {test: '(type \'(1 2 3))', result: new Sym('list')},
+        {test: '(type \'(1 . 2))', result: new Sym('list')},
+        {test: '(type +)', result: new Sym('subr')},
+        {test: '(type (lambda (x) x))', result: new Sym('usubr')},
+    ],
 
-QUnit.test('type', assert => {
-    tests.forEach(t => {
-        assert.deepEqual(evaluate(t.test), t.result, t.test)
-    })
-
-    errors.forEach(t => {
-        assert.throws(() => evaluate(t.test), t.result, t.test)
-    })
+    errors: [
+        {test: '(type)', result: new Error('type: too few arguments')},
+        {test: '(type 1 2)', result: new Error('type: too many arguments')},
+    ]
 })
